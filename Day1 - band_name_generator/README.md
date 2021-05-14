bc2b0233e77cb2804f8d155ffb1ffbe46e5f31f3
caglalogy <caglasahin180999@gmail.com>
2021-05-15 00:21:33 +0300

    day1 folder added

---
+++ Day1 - band_name_generator/README.md
@@ -0,0 +1,4 @@
+This exercise covers string manipulation and taking input from user in Python.
+
+in 'Day1 - band_name_generator' directory, write terminal the command below:
+py band_name_generator.py 
---
+++ Day1 - band_name_generator/band_name_generator.py
@@ -0,0 +1,9 @@
+print("""
+welcome to the band name generator!
+follow the instructions..
+""")
+city_name = input("enter the name of the city you grew up: ")
+fav_animal = input("enter you favourite animal: ")
+
+band_name = city_name + " " + fav_animal
+print("your band name is.. " + band_name)
