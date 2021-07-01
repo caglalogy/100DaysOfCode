b2b3634667806f1868196f7201f1f7d31ff81fa3
caglalogy <caglasahin180999@gmail.com>
2021-07-01 11:59:10 +0300

    updates

---
+++ Day20-29 Projects (cont.)/Day20 - Snake Game/snake.py
@@ -0,0 +1,47 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 20
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for pos in STARTING_POSITIONS:
+            new_seg = Turtle("square")
+            new_seg.color("white")
+            new_seg.penup()
+            new_seg.goto(pos)
+            self.segments.append(new_seg)
+
+    def move(self):
+        for seg_n in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_n - 1].xcor()
+            new_y = self.segments[seg_n - 1].ycor()
+            self.segments[seg_n].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
